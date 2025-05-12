c3a22be99f53755ad964f5515219fec97b9568fe
MR.RITU-R <riturajkumar1105@gmail.com>
2025-05-13 01:26:33 +0530

    Add files via upload

---
+++ sendEmail.js
@@ -0,0 +1,28 @@
+(function () {
+    emailjs.init({
+        publicKey: "wrzGPvG8Dq_aUT-zh",
+    });
+})();
+
+function sendEmail() {
+    var params = {
+        name: document.querySelector(".name").value,
+        email: document.querySelector(".email").value,
+        email: document.querySelector(".mobile").value,
+        sms: document.querySelector(".sms").value,
+    }
+    const serviceID = "service_o243fyw";
+    const templateID = "template_06srgch";
+    emailjs.send(serviceID, templateID, params)
+        .then(
+            (res) => {
+                document.querySelector(".name").value = "";
+                document.querySelector(".email").value = "";
+                document.querySelector(".mobile").value = "";
+                document.querySelector(".sms").value = "";
+                console.log(res);
+                console.log(sms);
+                alert("you sms send successfully.");
+            })
+        .catch((err) => console.log(err));
+}
